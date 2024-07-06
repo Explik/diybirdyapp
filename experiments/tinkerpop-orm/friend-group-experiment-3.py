7a104cdc081c3f8bf3c9db63e5ddfc7c213a944d
Explik <90565470+Explik@users.noreply.github.com>
2024-07-06 13:41:36 +0200

    Splitting traversal in smaller parts to enable reuse

--- experiments/tinkerpop-orm/friend-group-experiment-3.py
+++ experiments/tinkerpop-orm/friend-group-experiment-3.py
@@ -27,39 +27,59 @@ g.addV('person').property("id", 0).property('name', 'Charlie').property('age', 3
 
 # Setup classes 
 class Person: 
-    def __init__(self, vertex) -> None:
-        self.id: int = vertex['id']
-        self.name: str = vertex['name']
-        self.age: int = vertex['age']
-        self.city: str = vertex['city']
-        self.friend_ids = vertex['knows_id']
+    def __init__(self: int, id: int, name: str, age: int, city: str, knows_id: list[int]) -> None:
+        self.id = id
+        self.name = name
+        self.age = age
+        self.city = city
+        self.friend_ids = knows_id
 
-
-class FriendsRepository: 
+class PersonRepository: 
     def __init__(self, graph) -> None:
         self.graph = graph
     
-    def get_friend_group(self, name: str) -> list[Person]: 
-        vertices = self.graph.V().has('person', 'name', name).\
-            out('knows').aggregate('friends'). \
-            project('id', 'name', 'age', 'city', 'knows_id'). \
+    def get_person(self, name: str) -> Person: 
+        origin_vertex = self.graph.V().has('person', 'name', name)
+        projected_vertex = PersonRepository.project_person_vertices(origin_vertex).next()
+        return PersonRepository.map_person_vertex(projected_vertex)
+
+    def get_friend_group_of(self, name: str) -> list[Person]: 
+        origin_vertex = self.graph.V().has('person', 'name', name)
+        related_vertices = origin_vertex.out('knows').aggregate('friends')
+        projected_vertices = PersonRepository.project_person_vertices(related_vertices).toList()
+        modeled_vertices = [PersonRepository.map_person_vertex(v) for v in projected_vertices]
+
+        return modeled_vertices
+
+    def project_person_vertices(traversal): 
+        return traversal \
+            .project('id', 'name', 'age', 'city', 'knows_id'). \
                 by('id'). \
                 by('name'). \
                 by('age'). \
                 by('city'). \
-                by(__.out('knows').values('id').fold()). \
-            toList()
-        
-        people = [Person(v) for v in vertices]
+                by(__.out('knows').values('id').fold())
 
-        return people
+    def map_person_vertex(vertex):
+        return Person(
+            vertex['id'], 
+            vertex['name'], 
+            vertex['age'], 
+            vertex['city'], 
+            vertex['knows_id'])
         
 # Output the results 
-repository = FriendsRepository(g)
-friend_group = repository.get_friend_group('Charlie')
+repository = PersonRepository(g)
+charlie = repository.get_person('Charlie')
+charlie_friends = repository.get_friend_group_of('Charlie')
 
-for person in friend_group: 
+def print_friend(person): 
     print(f"Person {person.name} (Id: {person.id}, Age: {person.age}, City: {person.city}) knows: {person.friend_ids}")
+
+print_friend(charlie)
+print("has friends")
+for member in charlie_friends: 
+    print_friend(member)
 
 # Close the connection
 remoteConn.close()
