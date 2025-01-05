d893503417c392bc3f4542faa4c11831e8bce28e
Explik <90565470+Explik@users.noreply.github.com>
2025-01-05 18:21:06 +0100

    Adding vocabulary page and reworking navigation

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { ImportModulePageComponent } from './modules/style-library/import-module
 import { FlashcardDeckPageComponent } from './modules/flashcard/components/flashcard-deck-page/flashcard-deck-page.component';
 import { FlashcardDecksPageComponent } from './modules/flashcard/components/flashcard-decks-page/flashcard-decks-page.component';
 import { SessionPageComponent } from './modules/exercise/pages/session-page/session-page.component';
+import { VocabularyPageComponent } from './modules/flashcard/pages/vocabulary-page/vocabulary-page.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/flashcard-deck', pathMatch: 'full' },
   { path: 'flashcard-deck', component: FlashcardDecksPageComponent },
   { path: 'flashcard-deck/:id', component: FlashcardDeckPageComponent },
   { path: 'session/:id', component: SessionPageComponent },
   { path: 'exercise/:id', component: ExercisePageComponent },
-  { path: 'library/import', component: ImportModulePageComponent }
+  { path: 'library/import', component: ImportModulePageComponent },
+  { path: 'vocabulary', component: VocabularyPageComponent },
 ];
---
+++ web/src/app/modules/flashcard/pages/vocabulary-page/vocabulary-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { VocabularyService } from '../../services/vocabulary.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-vocabulary-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './vocabulary-page.component.html'
+})
+export class VocabularyPageComponent {
+  vocabulary?: VocabularyDto;
+
+  constructor(private service: VocabularyService) {}
+
+  ngOnInit() {
+    this.service.getVocabulary().subscribe(vocabulary => {
+      this.vocabulary = vocabulary;
+    });
+  }
+}
