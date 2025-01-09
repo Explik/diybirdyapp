c4118bcde96c1eeed050b7b1a164072f815a1337
Explik <90565470+Explik@users.noreply.github.com>
2025-01-09 10:12:02 +0100

    Making graph configurable and adding development secrets through application-[profile].properties

---
+++ api/README.md
@@ -0,0 +1,7 @@
+## Setup 
+Add application-dev.properties file in src/main/resources folder with the following content:
+```
+gremlin.server.host=localhost
+gremlin.server.protocol=websocket
+gremlin.server.port=8182
+```
--- api/src/main/java/com/explik/diybirdyapp/GraphConfig.java
+++ api/src/main/java/com/explik/diybirdyapp/GraphConfig.java
@@ -6,21 +6,29 @@ import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 import org.apache.tinkerpop.gremlin.util.ser.Serializers;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.apache.tinkerpop.gremlin.driver.Cluster;
 
-import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
 
 @Configuration
 public class GraphConfig {
+    @Value("${gremlin.server.host}")
+    private String host;
+
+    @Value("${gremlin.server.protocol}")
+    private String protocol;
+
+    @Value("${gremlin.server.port}")
+    private int port;
+
     @Bean
     public GraphTraversalSource traversalSource() {
         Cluster cluster = Cluster
-                .build("localhost")
-                .protocol("websocket")
-                .port(8182)
+                .build(host)
+                .protocol(protocol)
+                .port(port)
                 .serializer(Serializers.GRAPHSON_V3)
                 .create();
         return AnonymousTraversalSource.traversal().withRemote(DriverRemoteConnection.using(cluster, "g"));
