7a6c1a3220b0618b17a9447b957793ef6d44b6ab
Explik <90565470+Explik@users.noreply.github.com>
2025-01-15 16:02:22 +0100

    Adding dynamic flashcard to review exercise

--- web/src/app/modules/exercise/components/exercise-content-review-flashcard-container/exercise-content-review-flashcard-container.component.ts
+++ web/src/app/modules/exercise/components/exercise-content-review-flashcard-container/exercise-content-review-flashcard-container.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { FlashcardContent } from '../../../../shared/models/content.interface';
 import { InstructionComponent } from "../instruction/instruction.component";
 import { CommonModule } from '@angular/common';
 import { RecognizabilityRatingComponent } from "../../../../shared/components/recognizability-rating/recognizability-rating.component";
-import { FlashcardReviewComponent } from '../../../flashcard/components/flashcard-review/flashcard-review.component';
 import { ExerciseService } from '../../services/exercise.service';
 import { ExerciseSessionService } from '../../services/exerciseSession.service';
+import { DynamicFlashcardContentComponent } from '../dynamic-flashcard-content/dynamic-flashcard-content.component';
 
 @Component({
   selector: 'app-exercise-content-review-flashcard-container',
   standalone: true,
-  imports: [CommonModule, InstructionComponent, FlashcardReviewComponent, RecognizabilityRatingComponent],
+  imports: [CommonModule, InstructionComponent, DynamicFlashcardContentComponent, RecognizabilityRatingComponent],
   templateUrl: './exercise-content-review-flashcard-container.component.html'
 })
 export class ExerciseContentReviewFlashcardContainerComponent implements OnInit {
-  content?: FlashcardContent;
+  content?: ExerciseContentFlashcardDto;
 
   constructor(
     private exerciseService: ExerciseService,
@@ -22,7 +21,7 @@ export class ExerciseContentReviewFlashcardContainerComponent implements OnInit 
   ) { }
 
   ngOnInit(): void {
-    this.exerciseService.getContent<FlashcardContent>().subscribe(data => this.content = data);
+    this.exerciseService.getContent<ExerciseContentFlashcardDto>().subscribe(data => this.content = data);
   }
 
   async handleRatingSelected(rating: string) {
