ec349e0243cd0d4471fbfc2511861d240b463f7b
Explik <90565470+Explik@users.noreply.github.com>
2025-01-18 11:16:15 +0100

    More refactoring

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { ExercisePageComponent } from './modules/exercise/pages/exercise-page/exercise-page.component';
 import { ImportModulePageComponent } from './modules/style-library/import-module-page/import-module-page.component';
-import { FlashcardDeckPageComponent } from './modules/flashcard/components/flashcard-deck-page/flashcard-deck-page.component';
-import { FlashcardDecksPageComponent } from './modules/flashcard/components/flashcard-decks-page/flashcard-decks-page.component';
+import { FlashcardDeckPageComponent } from './modules/flashcard/pages/flashcard-deck-page/flashcard-deck-page.component';
+import { FlashcardDecksPageComponent } from './modules/flashcard/pages/flashcard-decks-page/flashcard-decks-page.component';
 import { SessionPageComponent } from './modules/exercise/pages/session-page/session-page.component';
 import { VocabularyPageComponent } from './modules/flashcard/pages/vocabulary-page/vocabulary-page.component';
 import { SharedComponentsPageComponent } from './shared/pages/shared-components-page/shared-components-page.component';
