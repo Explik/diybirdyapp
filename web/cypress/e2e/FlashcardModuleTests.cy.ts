e8586df9d4564babb66dd8aee003cfffc3facdda
Explik <90565470+Explik@users.noreply.github.com>
2025-03-28 12:53:13 +0100

    Fixing issue with left side input options not appearing on flashcard edit panel

--- web/cypress/e2e/FlashcardModuleTests.cy.ts
+++ web/cypress/e2e/FlashcardModuleTests.cy.ts
@@ -1,97 +1,147 @@
 describe('Flashcard deck features', () => {
-  it('Name is updated when name update is saved', () => {
-    createNewDeck(); 
+  describe('Flashcard deck data', () => {
+    it('Name is updated when name update is saved', () => {
+      createNewDeck(); 
 
-    setDeckName('Updated deck');
-    saveDeckChanges(); 
+      setDeckName('Updated deck');
+      saveDeckChanges(); 
 
-    // Check if name is updated on details page
-    cy.reload();
-    cy.contains('Updated deck').should('exist')
+      // Check if name is updated on details page
+      cy.reload();
+      cy.contains('Updated deck').should('exist')
 
-    // Check if name is updated on overview page
-    cy.visit('/flashcard-deck/')
-    cy.contains('Updated deck').should('exist')
+      // Check if name is updated on overview page
+      cy.visit('/flashcard-deck/')
+      cy.contains('Updated deck').should('exist')
+    }); 
+
+    it ('Name is not updated when name update is not saved', () => {
+      createNewDeck(); 
+
+      setDeckName('Updated deck');
+
+      // Check if name is not updated on details page
+      cy.reload();
+      cy.contains('Updated deck').should('not.exist')
+
+      // Check if name is not updated on overview page
+      cy.visit('/flashcard-deck/')
+      cy.contains('Updated deck').should('not.exist')
+    }); 
   }); 
 
-  it ('Name is not updated when name update is not saved', () => {
-    createNewDeck(); 
+  describe('Flashcard deck exercises', () => {
+    it ('Start exercises options are disabled when no flashcards are present', () => {
+      createNewDeck();
 
-    setDeckName('Updated deck');
+      cy.contains('Select flashcards').should('be.disabled')
+      cy.contains('Review flashcards').should('be.disabled')
+      cy.contains('Write flashcards').should('be.disabled')
+      cy.contains('Learn flashcards').should('be.disabled')
+    });
 
-    // Check if name is not updated on details page
-    cy.reload();
-    cy.contains('Updated deck').should('not.exist')
+    it ('Start exercises options are enabled when flashcards are present', () => {
+      createNewDeck(); 
 
-    // Check if name is not updated on overview page
-    cy.visit('/flashcard-deck/')
-    cy.contains('Updated deck').should('not.exist')
+      cy.contains('Add flashcard').click(); 
+      setLeftTextContent('Question 1')
+      setRightTextContent('Answer 1')
+      cy.contains('Save changes').click(); 
+
+      cy.contains('Select flashcards').should('not.be.disabled')
+      cy.contains('Review flashcards').should('not.be.disabled')
+      cy.contains('Write flashcards').should('not.be.disabled')
+      cy.contains('Learn flashcards').should('not.be.disabled')
+    });
   }); 
 
-  it ('Start exercises options are disabled when no flashcards are present', () => {
-    createNewDeck();
+  describe('Flashcard deck flashcards', () => {
+    it ('Flashcard is added when add flashcard is clicked', () => {
+      createNewDeck();
 
-    cy.contains('Select flashcards').should('be.disabled')
-    cy.contains('Review flashcards').should('be.disabled')
-    cy.contains('Write flashcards').should('be.disabled')
-    cy.contains('Learn flashcards').should('be.disabled')
+      createNewFlashcard(); 
+      
+      cy.get('.flashcard-edit-item').should('exist'); 
+    }); 
+
+    it ('Flashcard is removed when remove flashcard is clicked', () => {
+      createNewDeck();
+      createNewFlashcard(); 
+
+      deleteFlashcard();
+
+      cy.get('.flashcard-edit-item').should('not.exist');
+    }); 
+
+    it ('Flashcard is not persisted when not saved', () => {
+      createNewDeck();
+
+      createNewFlashcard(); 
+      setLeftTextContent('Question 1')
+      setRightTextContent('Answer 1')
+      cy.reload(); 
+
+      cy.contains('Question 1').should('not.exist'); 
+      cy.contains('Answer 1').should('not.exist');
+    }); 
+
+    it ('Text-text flaschards input options are available when selected', () => {
+      createNewDeck(); 
+
+      createNewFlashcard();
+      setLeftContentType('Text');
+      setRightContentType('Text');
+
+      cy.get(':nth-child(1) > .flashcard-edit-item .left-side .text-input-container').should('exist');
+      cy.get(':nth-child(1) > .flashcard-edit-item .right-side .text-input-container').should('exist');
+    }); 
+
+    it ('Audio-audio flaschards input options are available when selected', () => {
+      createNewDeck();
+
+      createNewFlashcard();
+      setLeftContentType('Audio');
+      setRightContentType('Audio');
+
+      cy.get(':nth-child(1) > .flashcard-edit-item .left-side .audio-input-container').should('exist');
+      cy.get(':nth-child(1) > .flashcard-edit-item .right-side .audio-input-container').should('exist');
+    });
+
+    it ('Image-image flaschards input options are available when selected', () => {
+      createNewDeck();
+
+      createNewFlashcard();
+      setLeftContentType('Image');
+      setRightContentType('Image');
+
+      cy.get(':nth-child(1) > .flashcard-edit-item .left-side .image-input-container').should('exist');
+      cy.get(':nth-child(1) > .flashcard-edit-item .right-side .image-input-container').should('exist');
+    }); 
+
+    it ('Video-video flaschards input options are available when selected', () => {
+      createNewDeck();
+
+      createNewFlashcard();
+      setLeftContentType('Video');
+      setRightContentType('Video');
+
+      cy.get(':nth-child(1) > .flashcard-edit-item .left-side .video-input-container').should('exist');
+      cy.get(':nth-child(1) > .flashcard-edit-item .right-side .video-input-container').should('exist');
+    }); 
+
+    it ('Text-text flashcard is persisted when saved', () => {
+      createNewDeck();
+
+      createNewFlashcard(); 
+      setLeftTextContent('Question 1')
+      setRightTextContent('Answer 1')
+      saveDeckChanges();
+
+      cy.reload(); 
+      cy.contains('Question 1').should('exist'); 
+      cy.contains('Answer 1').should('exist');
+    });   
   });
-
-  it ('Start exercises options are enabled when flashcards are present', () => {
-    createNewDeck(); 
-
-    cy.contains('Add flashcard').click(); 
-    setLeftTextContent('Question 1')
-    setRightTextContent('Answer 1')
-    cy.contains('Save changes').click(); 
-
-    cy.contains('Select flashcards').should('not.be.disabled')
-    cy.contains('Review flashcards').should('not.be.disabled')
-    cy.contains('Write flashcards').should('not.be.disabled')
-    cy.contains('Learn flashcards').should('not.be.disabled')
-  });
-
-  it ('Flashcard is added when add flashcard is clicked', () => {
-    createNewDeck();
-
-    createNewFlashcard(); 
-    
-    cy.get('flaschart-edit-item').should('exist'); 
-  }); 
-
-  it ('Flashcard is removed when remove flashcard is clicked', () => {
-    createNewDeck();
-    createNewFlashcard(); 
-
-    deleteFlashcard();
-
-    cy.get('flaschart-edit-item').should('not.exist');
-  }); 
-
-  it ('Text-text flashcard is persisted when saved', () => {
-    createNewDeck();
-
-    createNewFlashcard(); 
-    setLeftTextContent('Question 1')
-    setRightTextContent('Answer 1')
-    saveDeckChanges();
-
-    cy.reload(); 
-    cy.contains('Question 1').should('exist'); 
-    cy.contains('Answer 1').should('exist');
-  }); 
-
-  it ('Text-text flashcard is not persisted when not saved', () => {
-    createNewDeck();
-
-    createNewFlashcard(); 
-    setLeftTextContent('Question 1')
-    setRightTextContent('Answer 1')
-    cy.reload(); 
-
-    cy.contains('Question 1').should('not.exist'); 
-    cy.contains('Answer 1').should('not.exist');
-  }); 
 });
 
 // Flashcard deck functions 
@@ -119,12 +169,19 @@ function deleteFlashcard() {
 }
 
 function setLeftTextContent(content: string) {
-  cy.get(':nth-child(1) > .flashcard-edit-item .left-content-type-input').select('Text'); 
-  cy.get(':nth-child(1) > .flashcard-edit-item .left-side .text-input-container textarea').clear().type(content);
+  setLeftContentType('Text'); 
+  cy.get(':nth-child(1) > .flashcard-edit-item .left-side .text-input-container > textarea').clear().type(content);
 }
 
 function setRightTextContent(content: string) {
-  cy.get(':nth-child(1) > .flashcard-edit-item .right-content-type-input').select('Text');
+  setRightContentType('Text');
   cy.get(':nth-child(1) > .flashcard-edit-item .right-side .text-input-container > textarea').clear().type(content);
 } 
 
+function setLeftContentType(contentType: string) {
+  cy.get(':nth-child(1) > .flashcard-edit-item .left-content-type-input').select(contentType); 
+}
+
+function setRightContentType(contentType: string) {
+  cy.get(':nth-child(1) > .flashcard-edit-item .right-content-type-input').select(contentType); 
+}
