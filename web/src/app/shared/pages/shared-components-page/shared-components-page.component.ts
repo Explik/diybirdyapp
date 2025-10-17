6617dd72b5bea5e7e19d773eaeb27d5691117189
Explik <90565470+Explik@users.noreply.github.com>
2025-10-17 14:20:30 +0200

    Updating flashcard component to match design guide

--- web/src/app/shared/pages/shared-components-page/shared-components-page.component.ts
+++ web/src/app/shared/pages/shared-components-page/shared-components-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FlashcardComponent } from "../../components/flashcard/flashcard.component";
 import { IconComponent } from "../../components/icon/icon.component";
+import { ProgressBarComponent } from "../../components/progress-bar/progress-bar.component";
 
 @Component({
   selector: 'app-shared-components-page',
   standalone: true,
-  imports: [FlashcardComponent, IconComponent],
+  imports: [FlashcardComponent, IconComponent, ProgressBarComponent],
   templateUrl: './shared-components-page.component.html'
 })
 export class SharedComponentsPageComponent { }
