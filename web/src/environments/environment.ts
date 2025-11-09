bed672501186a6c9d3647d8002f0f63100274062
Explik <90565470+Explik@users.noreply.github.com>
2025-11-09 13:02:16 +0100

    Updated apiUrl

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080'  // Replace with your development API URL
+    apiUrl: '/api'  // Replace with your development API URL
 };
