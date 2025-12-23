c2c1a5337f2b490337c659ed6f61cecc2e9e8466
Explik <90565470+Explik@users.noreply.github.com>
2025-12-23 22:39:30 +0100

    Add endpoint to upload transcription

--- api/src/main/java/com/explik/diybirdyapp/service/TextContentService.java
+++ api/src/main/java/com/explik/diybirdyapp/service/TextContentService.java
@@ -1,6 +1,10 @@
 package com.explik.diybirdyapp.service;
 
 import com.explik.diybirdyapp.model.FileContentCommandResult;
+import com.explik.diybirdyapp.model.content.TextContentTranscriptionDto;
+import com.explik.diybirdyapp.persistence.command.CreateTranscriptionCommand;
+import com.explik.diybirdyapp.persistence.command.CreateTranscriptionSystemCommand;
+import com.explik.diybirdyapp.persistence.command.handler.CommandHandler;
 import com.explik.diybirdyapp.service.storageService.BinaryStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,6 +18,12 @@ public class TextContentService {
 
     @Autowired
     AudioContentService audioContentService;
+
+    @Autowired
+    CommandHandler<CreateTranscriptionSystemCommand> createTranscriptionSystemCommandHandler;
+
+    @Autowired
+    CommandHandler<CreateTranscriptionCommand> createTranscriptionCommandHandler;
 
     public FileContentCommandResult getPronunciation(String id) {
         var audio = audioContentService.getAudioForTextContent(id);
@@ -33,5 +43,20 @@ public class TextContentService {
             binaryStorageService.delete(newFileName);
             throw new RuntimeException("Failed to add audio for text content", e);
         }
+    }
+
+    public void addTranscription(String id, TextContentTranscriptionDto transcription) {
+        // Create the transcription system if it doesn't exist
+        var createSystemCommand = new CreateTranscriptionSystemCommand();
+        createSystemCommand.setId(transcription.getTranscriptionSystem());
+        createTranscriptionSystemCommandHandler.handle(createSystemCommand);
+
+        // Create the transcription
+        var createTranscriptionCommand = new CreateTranscriptionCommand();
+        createTranscriptionCommand.setId(UUID.randomUUID().toString());
+        createTranscriptionCommand.setSourceContentId(id);
+        createTranscriptionCommand.setTextValue(transcription.getTranscription());
+        createTranscriptionCommand.setTranscriptionSystemId(transcription.getTranscriptionSystem());
+        createTranscriptionCommandHandler.handle(createTranscriptionCommand);
     }
 }
