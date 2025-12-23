1c9ad98d94f5f272177b88caab6aa300408f9cdb
Explik <90565470+Explik@users.noreply.github.com>
2025-12-23 11:36:42 +0100

    Replacing exercise answer vertex with command

--- api/src/main/java/com/explik/diybirdyapp/persistence/command/handler/CreateExerciseAnswerRecognizabilityRatingCommandHandler.java
+++ api/src/main/java/com/explik/diybirdyapp/persistence/command/handler/CreateExerciseAnswerRecognizabilityRatingCommandHandler.java
@@ -1,48 +1,27 @@
 package com.explik.diybirdyapp.persistence.command.handler;
 
 import com.explik.diybirdyapp.persistence.command.CreateExerciseAnswerRecognizabilityRatingCommand;
-import com.explik.diybirdyapp.persistence.command.CreateRecognizabilityRatingVertexCommand;
-import com.explik.diybirdyapp.persistence.vertex.ExerciseAnswerVertex;
-import com.explik.diybirdyapp.persistence.vertex.ExerciseSessionVertex;
-import com.explik.diybirdyapp.persistence.vertex.ExerciseVertex;
-import com.explik.diybirdyapp.persistence.vertex.RecognizabilityRatingVertex;
+import com.explik.diybirdyapp.persistence.command.helper.ExerciseAnswerCommandHelper;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class CreateExerciseAnswerRecognizabilityRatingCommandHandler implements CommandHandler<CreateExerciseAnswerRecognizabilityRatingCommand> {
     private final GraphTraversalSource traversalSource;
-    private final CommandHandler<CreateRecognizabilityRatingVertexCommand> createRecognizabilityRatingVertexCommandHandler;
 
-    public CreateExerciseAnswerRecognizabilityRatingCommandHandler(
-            @Autowired GraphTraversalSource traversalSource,
-            @Autowired CommandHandler<CreateRecognizabilityRatingVertexCommand> createRecognizabilityRatingVertexCommandHandler) {
+    public CreateExerciseAnswerRecognizabilityRatingCommandHandler(@Autowired GraphTraversalSource traversalSource) {
         this.traversalSource = traversalSource;
-        this.createRecognizabilityRatingVertexCommandHandler = createRecognizabilityRatingVertexCommandHandler;
     }
 
     @Override
     public void handle(CreateExerciseAnswerRecognizabilityRatingCommand command) {
-        var exerciseVertex = ExerciseVertex.getById(traversalSource, command.getExerciseId());
-        var sessionVertex = ExerciseSessionVertex.findById(traversalSource, command.getSessionId());
-
-        // Create recognizability rating
-        var ratingId = UUID.randomUUID().toString();
-        var createRatingCommand = new CreateRecognizabilityRatingVertexCommand();
-        createRatingCommand.setId(ratingId);
-        createRatingCommand.setRating(command.getRating());
-        createRecognizabilityRatingVertexCommandHandler.handle(createRatingCommand);
-        var ratingVertex = RecognizabilityRatingVertex.findById(traversalSource, ratingId);
-
-        // Create exercise answer vertex
-        var answerId = (command.getId() != null) ? command.getId() : UUID.randomUUID().toString();
-        var answerVertex = ExerciseAnswerVertex.create(traversalSource);
-        answerVertex.setId(answerId);
-        answerVertex.setExercise(exerciseVertex);
-        answerVertex.setSession(sessionVertex);
-        answerVertex.setRecognizabilityRating(ratingVertex);
+        ExerciseAnswerCommandHelper.createWithRecognizabilityRating(
+                traversalSource,
+                command.getId(),
+                command.getExerciseId(),
+                command.getSessionId(),
+                command.getRating()
+        );
     }
 }
--- api/src/main/java/com/explik/diybirdyapp/persistence/command/handler/CreateExerciseAnswerTextCommandHandler.java
+++ api/src/main/java/com/explik/diybirdyapp/persistence/command/handler/CreateExerciseAnswerTextCommandHandler.java
@@ -1,64 +1,27 @@
 package com.explik.diybirdyapp.persistence.command.handler;
 
 import com.explik.diybirdyapp.persistence.command.CreateExerciseAnswerTextCommand;
-import com.explik.diybirdyapp.persistence.command.CreateTextContentVertexCommand;
-import com.explik.diybirdyapp.persistence.vertex.*;
+import com.explik.diybirdyapp.persistence.command.helper.ExerciseAnswerCommandHelper;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class CreateExerciseAnswerTextCommandHandler implements CommandHandler<CreateExerciseAnswerTextCommand> {
     private final GraphTraversalSource traversalSource;
-    private final CommandHandler<CreateTextContentVertexCommand> createTextContentVertexCommandHandler;
 
-    public CreateExerciseAnswerTextCommandHandler(
-            @Autowired GraphTraversalSource traversalSource,
-            @Autowired CommandHandler<CreateTextContentVertexCommand> createTextContentVertexCommandHandler) {
+    public CreateExerciseAnswerTextCommandHandler(@Autowired GraphTraversalSource traversalSource) {
         this.traversalSource = traversalSource;
-        this.createTextContentVertexCommandHandler = createTextContentVertexCommandHandler;
     }
 
     @Override
     public void handle(CreateExerciseAnswerTextCommand command) {
-        var exerciseVertex = ExerciseVertex.getById(traversalSource, command.getExerciseId());
-        var sessionVertex = ExerciseSessionVertex.findById(traversalSource, command.getSessionId());
-
-        // Determine language from exercise content
-        LanguageVertex languageVertex;
-        var content = exerciseVertex.getContent();
-        if (content instanceof TextContentVertex textContentVertex) {
-            languageVertex = textContentVertex.getLanguage();
-        }
-        else if (content instanceof FlashcardVertex flashcardVertex){
-            var flashcardSide = exerciseVertex.getFlashcardSide();
-            var textContentVertex = (TextContentVertex)flashcardVertex.getSide(flashcardSide);
-            languageVertex = textContentVertex.getLanguage();
-        }
-        else if (content instanceof AudioContentVertex) {
-            throw new RuntimeException("Audio content not supported for text answers");
-        }
-        else {
-            throw new RuntimeException("Unsupported content type for exercise: " + content.getClass().getSimpleName());
-        }
-
-        // Create text content for the answer
-        var textId = UUID.randomUUID().toString();
-        var createTextCommand = new CreateTextContentVertexCommand();
-        createTextCommand.setId(textId);
-        createTextCommand.setValue(command.getText());
-        createTextCommand.setLanguage(languageVertex.getId());
-        createTextContentVertexCommandHandler.handle(createTextCommand);
-        var textVertex = TextContentVertex.findById(traversalSource, textId);
-
-        // Create exercise answer vertex
-        var answerId = (command.getId() != null) ? command.getId() : UUID.randomUUID().toString();
-        var exerciseAnswerVertex = ExerciseAnswerVertex.create(traversalSource);
-        exerciseAnswerVertex.setId(answerId);
-        exerciseAnswerVertex.setExercise(exerciseVertex);
-        exerciseAnswerVertex.setSession(sessionVertex);
-        exerciseAnswerVertex.setContent(textVertex);
+        ExerciseAnswerCommandHelper.createWithTextContent(
+                traversalSource,
+                command.getId(),
+                command.getExerciseId(),
+                command.getSessionId(),
+                command.getText()
+        );
     }
 }
