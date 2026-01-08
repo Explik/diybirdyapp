8864eb176160d9ab034ee53df5352a9b8c791bf9
Explik <90565470+Explik@users.noreply.github.com>
2026-01-08 17:27:33 +0100

    Adding hash location to play nicely with Digital Ocean Routing

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { FormsModule } from '@angular/forms';
@@ -11,7 +11,7 @@ import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(withInterceptorsFromDi()),
     CommonModule,
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
