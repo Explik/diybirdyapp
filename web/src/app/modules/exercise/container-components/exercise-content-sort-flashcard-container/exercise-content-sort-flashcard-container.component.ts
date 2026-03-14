7a5103a83e5c204df2585ee0b18e63445bf43cf1
Explik <90565470+Explik@users.noreply.github.com>
2026-03-14 10:53:50 +0100

    Adding hotkeys to sort-flashcard-exercise input

--- web/src/app/modules/exercise/container-components/exercise-content-sort-flashcard-container/exercise-content-sort-flashcard-container.component.ts
+++ web/src/app/modules/exercise/container-components/exercise-content-sort-flashcard-container/exercise-content-sort-flashcard-container.component.ts
@@ -1,27 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { InstructionComponent } from '../../components/instruction/instruction.component';
 import { DynamicFlashcardContentComponent } from '../../components/dynamic-flashcard-content/dynamic-flashcard-content.component';
 import { ExerciseService } from '../../services/exercise.service';
 import { ExerciseContentFlashcardDto } from '../../../../shared/api-client';
+import { HotkeyService } from '../../../../shared/services/hotKey.service';
+import { Subscription } from 'rxjs';
+import { IconComponent } from '../../../../shared/components/icon/icon.component';
 
 @Component({
   selector: 'app-exercise-content-sort-flashcard-container',
   standalone: true,
-  imports: [InstructionComponent, DynamicFlashcardContentComponent],
+  imports: [InstructionComponent, DynamicFlashcardContentComponent, IconComponent],
   templateUrl: './exercise-content-sort-flashcard-container.component.html'
 })
-export class ExerciseContentSortFlashcardContainerComponent implements OnInit {
-  content?: ExerciseContentFlashcardDto;
+export class ExerciseContentSortFlashcardContainerComponent implements OnInit, OnDestroy {
+  private subs = new Subscription();
 
-  constructor(private exerciseService: ExerciseService) {}
+  content?: ExerciseContentFlashcardDto;
+  isSubmitting = false;
+
+  constructor(
+    private exerciseService: ExerciseService,
+    private hotkeyService: HotkeyService
+  ) {}
 
   ngOnInit(): void {
     this.exerciseService.getContent<ExerciseContentFlashcardDto>().subscribe(data => this.content = data);
+
+    this.subs.add(
+      this.hotkeyService.onHotkey({ key: 'left' }).subscribe(() => {
+        void this.handlePileSelected('dont-know');
+      })
+    );
+
+    this.subs.add(
+      this.hotkeyService.onHotkey({ key: 'right' }).subscribe(() => {
+        void this.handlePileSelected('know');
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 
   async handlePileSelected(pile: string) {
-    await this.exerciseService.submitAnswerAsync({ type: 'sort-options', pile });
-    await this.exerciseService.nextExerciseAsync();
+    if (!this.content || this.isSubmitting) {
+      return;
+    }
+
+    this.isSubmitting = true;
+    try {
+      await this.exerciseService.submitAnswerAsync({ type: 'sort-options', pile });
+      await this.exerciseService.nextExerciseAsync();
+    } finally {
+      this.isSubmitting = false;
+    }
   }
 }
